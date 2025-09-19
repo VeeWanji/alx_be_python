51c04c83301cceaca0d60b6f8be52f60177efe21
Veronica Wanjiru <veronicashiroh@gmail.com>
2025-09-19 12:21:11 +0300

    Add multiplication_table.py - prints 1..11 multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,7 @@
 number = int(input("Enter a number to generate its multiplication table: "))
+except ValueError:
+    print("Please enter a valid integer.")
+    exit(1)
 for i in range(1, 11):
     print(f"{number} * {i} = {number * i}") 
 
