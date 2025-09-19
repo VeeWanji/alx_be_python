fe93238effa28a66c7d5031b793797435235b92f
Veronica Wanjiru <veronicashiroh@gmail.com>
2025-09-19 11:45:14 +0300

    Add match_case_calculator.py using match case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+match operation:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+    case _:
+        print("Invalid operation selected.")
+        
