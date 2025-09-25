15c83d4ae670a3f3dd275fc16cf3235bbb8d84fd
Veronica Wanjiru <veronicashiroh@gmail.com>
2025-09-25 17:33:08 +0300

    Add explore_datetime.py script to demonstrate datetime usage

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,12 @@
+def display_current_datetime():
+    current_date = datetime.now()
+    print("Current date and time:", current_date.strftime("YYYY-MM-DD HH:MM:SS"))"))
+    return current_date
+
+
+def calculate_future_date(current_date, days_to_add):
+    future_date = current_date + timedelta(days=days_to_add)
+    print("Future date:", future_date.strftime("YYYY-MM-DD))"))
+    return future_date
+
+if _name_ == "_main_":
